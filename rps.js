3696473c7e7b51e3c1fc8f4f8c518a3128dbce0a
Olanibi Gideon Adeshina <holargee123@gmail.com>
2023-04-26 15:34:06 +0100

    Update rps.js

--- rps.js
+++ rps.js
@@ -11,11 +11,14 @@ var rpaper = document.getElementById("rpaper");
 var rscissors = document.getElementById("rscissors");
 var rad = document.getElementById("rad");
 var win = document.getElementById("win");
+var pas = document.getElementById("pas");
+var scis = document.getElementById("scis");
+var rocs = document.getElementById("rocs");
 setInterval(function(){
    if(tri.style.display=="block"){
-      paper.innerText="🖐️";
-      rock.innerText="✊";
-      scissors.innerText="✌️";
+      pas.innerHTML="&#9995;";
+      rocs.innerHTML="&#9994;";
+      scis.innerHTML="&#9996;";
 } 
 
 },50)
@@ -29,7 +32,7 @@ function clos(){
 document.getElementById("body1").style.filter="blur(0px)";
 document.getElementById("rulet").style.display="none";    
 }
-rock.addEventListener("click" , function(){wi();random();});
+rock.addEventListener("click" , function(){;wi();random();});
 scissors.addEventListener("click" , function(){wi();random()});
 paper.addEventListener("click" , function(){wi();random()});
 
@@ -38,17 +41,16 @@ if (rad.checked==true){
  setTimeout(wine,200);}
 }
 function wine(){
-//alert("work");
-   if(paper.innerText=="🖐️" && scissors.innerText=="🖐️"){win.innerText="DRAW🤝"}
-else if(paper.innerText=="🖐️" && scissors.innerText=="✊"){
-win.innerText="WIN🎖️";score = score - -1;document.getElementById("score").value=score} 
- else if(paper.innerText=="🖐️" && scissors.innerText=="✌️"){win.innerText="LOSE💔"}
-else if(paper.innerText=="✌️" && scissors.innerText=="✌️"){win.innerText="DRAW🤝"}
-else if(paper.innerText=="✌️" && scissors.innerText=="✊"){win.innerText="LOSE💔"}
-else if(paper.innerText=="✌️" && scissors.innerText=="🖐️"){win.innerText="WIN🎖️";score = score - -1;document.getElementById("score").value=score}
-else if(paper.innerText=="✊" && scissors.innerText=="✊"){win.innerText="DRAW🤝"}
-else if(paper.innerText=="✊" && scissors.innerText=="✌️"){win.innerText="WIN🎖️";score = score - -1;document.getElementById("score").value=score}
-else if(paper.innerText=="✊" && scissors.innerText=="🖐️"){win.innerText="LOSE💔"}  
+ if(pas.innerText==String.fromCharCode(9995) && scis.innerText==String.fromCharCode(9995)){win.innerHTML="DRAW &#129309;"}
+else if(pas.innerText==String.fromCharCode(9995) && scis.innerText==String.fromCharCode(9994)){
+win.innerHTML="WIN &#127894;";score = score - -1;document.getElementById("score").value=score} 
+ else if(pas.innerText==String.fromCharCode(9995) && scis.innerText==String.fromCharCode(9996)){win.innerHTML="LOSE &#128148;"}
+else if(pas.innerText==String.fromCharCode(9996) && scis.innerText==String.fromCharCode(9996)){win.innerHTML="DRAW &#129309;"}
+else if(pas.innerText==String.fromCharCode(9996) && scis.innerText==String.fromCharCode(9994)){win.innerHTML="LOSE &#128148;"}
+else if(pas.innerText==String.fromCharCode(9996) && scis.innerText==String.fromCharCode(9995)){win.innerHTML="WIN &#127894;";score = score - -1;document.getElementById("score").value=score}
+else if(pas.innerText==String.fromCharCode(9994) && scis.innerText==String.fromCharCode(9994)){win.innerHTML="DRAW &#129309;"}
+else if(pas.innerText==String.fromCharCode(9994) && scis.innerText==String.fromCharCode(9996)){win.innerHTML="WIN &#127894;";score = score - -1;document.getElementById("score").value=score}
+else if(pas.innerText==String.fromCharCode(9994) && scis.innerText==String.fromCharCode(9995)){win.innerHTML="LOSE &#128148;"}  
 }
 function switc() {
 rock.style.display = "none";
@@ -72,23 +74,23 @@ rad.checked=true;
 }
 function pape(){
 if (rad.checked==true){
-    paper.innerText="🖐️";
+    pas.innerHTML="&#9995;";
 }}
 function sci(){
 if (rad.checked==true){
-    paper.innerText="✌️";
+    pas.innerHTML="&#9996;";
 }}
 function roc(){
 if (rad.checked==true){
-  paper.innerText="✊";
+  pas.innerHTML="&#9994;";
 }}
 function random(){
 //alert("random");  
 if (rad.checked==true){
  var ran = Math.floor(Math.random() * 3);
- if (ran=="0"){scissors.innerText="🖐️"};
- if (ran=="1"){scissors.innerText="✊"};
- if (ran=="2"){scissors.innerText="✌️"};
+ if (ran=="0"){scis.innerHTML="&#9995;"};
+ if (ran=="1"){scis.innerHTML="&#9994;"};
+ if (ran=="2"){scis.innerHTML="&#9996;"};
 //document.getElementById("score").innerText=ran;
 }
 }
